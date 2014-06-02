25cff2aa829c68b4f9f3e949d6d7dfdfbea742dd
blackkitty <vs@natal.is>
2014-06-02 13:58:24 +0400

    123

--- Source1.f90
+++ Source1.f90
@@ -1,28 +1,16 @@
 ! Privet!
-character header(1:2)
-integer*4 id, userid, tmp
-!character*10 userid
-character*100000 metadata
+character header(1:4)
+integer*4 id, userid
+character*100000 data
 character*50 dataHash
 
 
 
-open(100, file = 'C:\FILES\EASYTEN\2014MAY\addedVia\addedVia02.txt')
+open(100, file = 'C:\MSDEV\Projects\addedVia\addedVia\files\addedVia01.txt')
+read(100,*), header(1:4)
+read(100,*), id, userid, data, dataHash
+write(*,*),id, userid, data(1:10), dataHash
 
-do i = 1, 9999
-write(*,*),i
-read(100,*), metadata
-tmp = SCAN (metadata,'addedVia')
-write(*,*), metadata(1:30), tmp
-
-if (tmp > 0) then
-    if (metadata(tmp:tmp+7) .eq. 'addedVia') then
-    write(*,*), metadata(tmp:tmp+7)
-    pause
-    endif
-endif
-
-enddo
 
 close(100)
 
